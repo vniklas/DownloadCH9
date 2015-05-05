370b513ea2aaadf6bfadce747c2e84c9fd019d3f
vniklas <niklas@djungeln.se>
2015-05-05 09:36:05 -0500

    updated with categories

--- DownloadMSIgniteBITS.ps1
+++ DownloadMSIgniteBITS.ps1
@@ -1,12 +1,20 @@
 ﻿# Download MS Ignite Content with BITS
 # 
-# Niklas Akerlund v 0.3 2015-05-03
+# Niklas Akerlund v 0.4 2015-05-05
 # 
 # Borrowed code for making the folders from Peter Schmidt (Exchange MVP, blog: www.msdigest.net) DownloadTechEdEurope14VideoAndSlides.ps1
 #
 [CmdletBinding()]
 param(
   [switch]$CH9,
+  [switch]$KEY,
+  [switch]$FDN,
+  [switch]$DEV,
+  [switch]$HYBRID,
+  [switch]$VOICE,
+  [switch]$CLOUD,
+  [switch]$IAAS,
+
   [switch]$PPT,
   [switch]$MP4,
   [string]$Dest='C:\Ignitetest')
@@ -21,9 +29,38 @@ $vsessions = @()
 #$ = 'C:\techedtest'
 
 if($CH9){
-  $psessions =  Invoke-RestMethod 'http://s.ch9.ms/Events/Ignite/2015/RSS/slides'  | where comments -cmatch "C9"
-  $vsessions =  Invoke-RestMethod 'http://s.ch9.ms/Events/Ignite/2015/RSS/mp4high'  | where comments -cmatch "C9"
+  $psessions =  Invoke-RestMethod 'http://s.ch9.ms/Events/Ignite/2015/RSS/slides'  | Where-Object comments -cmatch "C9"
+  $vsessions =  Invoke-RestMethod 'http://s.ch9.ms/Events/Ignite/2015/RSS/mp4high'  | Where-Object comments -cmatch "C9"
 }
+if($KEY){
+  $psessions += Invoke-RestMethod 'http://s.ch9.ms/Events/Ignite/2015/RSS/slides' | Where-Object comments -cmatch "KEY"
+  $vsessions += Invoke-RestMethod 'http://s.ch9.ms/Events/Ignite/2015/RSS/mp4high' | Where-Object comments -cmatch "KEY"
+}
+if($FDN){
+  $psessions += Invoke-RestMethod 'http://s.ch9.ms/Events/Ignite/2015/RSS/slides' | Where-Object comments -cmatch "FDN"
+  $vsessions += Invoke-RestMethod 'http://s.ch9.ms/Events/Ignite/2015/RSS/mp4high' | Where-Object comments -cmatch "FDN"
+}
+if($DEV){
+  $psessions += Invoke-RestMethod 'http://s.ch9.ms/Events/Ignite/2015/RSS/slides' | Where-Object category -Contains "development"
+  $vsessions += Invoke-RestMethod 'http://s.ch9.ms/Events/Ignite/2015/RSS/mp4high' | Where-Object category -Contains "development"
+}
+if($HYBRID){
+  $psessions += Invoke-RestMethod 'http://s.ch9.ms/Events/Ignite/2015/RSS/slides' | Where-Object category -Contains "hybrid"
+  $vsessions += Invoke-RestMethod 'http://s.ch9.ms/Events/Ignite/2015/RSS/mp4high' | Where-Object category -Contains "hybrid"
+}
+if($VOICE){
+  $psessions += Invoke-RestMethod 'http://s.ch9.ms/Events/Ignite/2015/RSS/slides' | Where-Object category -Contains "voice"
+  $vsessions += Invoke-RestMethod 'http://s.ch9.ms/Events/Ignite/2015/RSS/mp4high' | Where-Object category -Contains "voice"
+}
+if($CLOUD){
+  $psessions += Invoke-RestMethod 'http://s.ch9.ms/Events/Ignite/2015/RSS/slides' | Where-Object category -Contains "cloud"
+  $vsessions += Invoke-RestMethod 'http://s.ch9.ms/Events/Ignite/2015/RSS/mp4high' | Where-Object category -Contains "cloud"
+}
+if($IAAS){
+  $psessions += Invoke-RestMethod 'http://s.ch9.ms/Events/Ignite/2015/RSS/slides' | Where-Object category -Contains "infrastructure-as-a-service"
+  $vsessions += Invoke-RestMethod 'http://s.ch9.ms/Events/Ignite/2015/RSS/mp4high' | Where-Object category -Contains "infrastructure-as-a-service"
+}
+
 
 if ($All){
   $psessions =  Invoke-RestMethod 'http://s.ch9.ms/Events/Ignite/2015/RSS/slides' 
@@ -35,7 +72,7 @@ if ($All){
 if($PPT){
   foreach ($psession in $psessions){
       # create folder
-      $code = $psession.comments.split("/") | select -last 1	
+      $code = $psession.comments.split("/") | Select-Object -last 1	
       $folder = $code + " - " + $psession.title.Replace(":", "-").Replace("?", "").Replace("/", "-").Replace("<", "").Replace("|", "").Replace('"',"").Replace("*","")
 		  $folder = $folder.substring(0, [System.Math]::Min(100, $folder.Length))
 		  $folder = $folder.trim()
@@ -54,7 +91,7 @@ if($PPT){
 }
 if($MP4){
   foreach ($vsession in $vsessions){
-      $code = $vsession.comments.split("/") | select -last 1	
+      $code = $vsession.comments.split("/") | Select-Object -last 1	
       $folder = $code + " - " + $vsession.title.Replace(":", "-").Replace("?", "").Replace("/", "-").Replace("<", "").Replace("|", "").Replace('"',"").Replace("*","")
 		  $folder = $folder.substring(0, [System.Math]::Min(100, $folder.Length))
 		  $folder = $folder.trim()
